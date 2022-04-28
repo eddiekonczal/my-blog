2835038c7d2f3f97a578db9a0ad1b396b4aeabcd
Eddie Konczal <konczal@sas.rutgers.edu>
2022-04-27 20:22:13 -0400

    First commit

---
+++ src/build/precache-manifest.2d41618be4be5fc8ac24cd42f3a1cdf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b6f41f4e97e45f2a42c",
+    "url": "/static/css/main.51d39757.chunk.css"
+  },
+  {
+    "revision": "3b6f41f4e97e45f2a42c",
+    "url": "/static/js/main.34f90c4e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e1ce3b1012b19ecb889e",
+    "url": "/static/js/2.6eb2dbf2.chunk.js"
+  },
+  {
+    "revision": "55b75e3fda312a9d5db4808f81450c78",
+    "url": "/index.html"
+  }
+];
